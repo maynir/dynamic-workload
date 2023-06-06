5ef656dba3eb3a0556037ca4b3941cd75130361b
May Nir <maynirrr@gmail.com>
2023-06-06 13:19:42 +0300

    web_servers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 const fs = require('fs');
+const WorkerNode = require('./workerNode');
+const crypto = require('crypto');
+
 const app = express()
 const port = 5000
 
@@ -8,16 +11,43 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let workQueue = []
+let CompleteWorkQueue = []
+let numOfCurrentWorkers = 0
+let nextWorkId = 1;
+
+const maxNumOfWorkers = 0
+
 app.get('/', (req, res) => {
   res.send("Hello World, this is May's Dynamic Workload project!")
 })
 
 app.put('/enqueue', (req, res) => {
   const iterations = req.query.iterations;
+  const data = req.body;
+  const workId = ++nextWorkId;
+
+  const workItem = {
+    id: workId,
+    data,
+    iterations: parseInt(iterations),
+  };
+
+  workQueue.push(workItem);
+
+  // TODO: Add async workflow to check if work is handeled by worker
+
+  res.json({ id: workId });
 });
 
 app.post('/pullCompleted', (req, res) => {
   const top = req.query.top;
+  const numItems = parseInt(top);
+
+
+  const latestCompletedWork = CompleteWorkQueue.slice(-numItems);
+
+  res.json(latestCompletedWork);
 });
 
 app.listen(port, () => {
